10f059f56a0966c06dfb7f1d014dfafa167a8669
Harshana_P_Semasinghe <harshanaprabhathsemasinghe@gmail.com>
2024-02-21 01:05:31 +0530

    "Update the generateImagesFunctions"

--- UtilFunctions/index.ts
+++ UtilFunctions/index.ts
@@ -52,10 +52,13 @@ export const calculateRent = (city_mpg: number, year : number)=>{
 }
 
 export const generateCarImageUrl = (car: car, angle?: string) => {
+
+    
+
     const url = new URL("https://cdn.imagin.studio/getimage");
     const { make, model, year } = car;
   
-    url.searchParams.append('customer','hrjavascript-mastery');
+    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
     url.searchParams.append('make', make);
     url.searchParams.append('modelFamily', model.split(" ")[0]);
     url.searchParams.append('zoomType', 'fullscreen');
--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     images: {
         domains: ["cdn.imagin.studio"]
     }
+    
 
 };
 
